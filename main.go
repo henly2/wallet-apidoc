405fb2d20933c27f9762175172d0b5611f6cc416
henly.liu <henly.liu@blockshine.com>
2018-05-16 14:47:51 +0800

    增加yaml配置文件

--- main.go
+++ main.go
@@ -8,12 +8,62 @@ import (
 	"fmt"
 	"api_router/base/data"
 	"bastionpay_api/apigroup"
+	"bastionpay_api/gateway"
+	"flag"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"os"
+	"github.com/gin-contrib/cors"
 )
 
+var confFile = flag.String("c", "config.yml", "conf file.")
+
+type Config struct{
+	Server struct {
+		Port string `yaml:"port"`
+	}
+	Wallet struct {
+		Dir    		string `yaml:"dir"`
+		CfgName    	string `yaml:"cfgName"`
+	}
+}
 func main()  {
+	flag.Parse()
+
+	conf := new(Config)
+	data, err := ioutil.ReadFile(*confFile)
+	if err != nil {
+		fmt.Println("read yml config file error:", err.Error())
+		os.Exit(1)
+	}
+
+	err = yaml.Unmarshal([]byte(data), conf)
+	if err != nil {
+		fmt.Println("Unmarshal yml config file error:", err.Error())
+		os.Exit(1)
+	}
+
+	err = gateway.Init(conf.Wallet.Dir, conf.Wallet.CfgName)
+	if err != nil {
+		fmt.Println("gateway.Init error:", err.Error())
+		os.Exit(1)
+	}
+
 	engine := gin.Default()
+	engine.Use(cors.New(cors.Config{
+		AllowAllOrigins:true,
+		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Authorization", "X-Requested-With", "X_Requested_With", "Content-Type", "Access-Token", "Accept-Language"},
+		//AllowOrigins:     []string{"*"},
+		//AllowCredentials: true,
+		//AllowOriginFunc: func(origin string) bool {
+		//	return true;//origin == "https://github.com"
+		//},
+		//MaxAge: 12 * time.Hour,
+	}))
+
 	startSwagger(engine)
-	engine.Run(":8040")
+	engine.Run(":" + conf.Server.Port)
 }
 
 func DocLoader(key string) ([]byte, error){
@@ -26,8 +76,6 @@ func startSwagger(engine *gin.Engine)  {
 	config.Title = "BastionPay Api"
 	config.Description  = "BastionPay Api"
 	config.DocVersion = "1.0"
-	//config.SwaggerUrlPrefix = docRelativePath
-	//config.SwaggerUiUrl = "http://127.0.0.1:8030"
 	swagger.InitializeApiRoutes(engine, &config, DocLoader)
 
 	initDoc := func(userLevel int){
@@ -57,30 +105,42 @@ func startSwagger(engine *gin.Engine)  {
 		})
 
 		apiAll := apigroup.ListApiGroup()
-		for _, apiGroup := range apiAll {
-			for _, apiProxy := range apiGroup{
+		for _, apiGroupAll := range apiAll {
+			for _, apiProxy := range apiGroupAll{
 				if apiProxy.ApiDocInfo.Level > userLevel {
 					continue
 				}
 				router.POST(apiProxy.ApiDocInfo.Path, func(ctx *gin.Context) {
 					path := ctx.Request.URL.Path
+					path = strings.TrimRight(path, "/")
+					path = strings.TrimLeft(path, "/")
+
 					paths := strings.Split(path, "/")
 					if len(paths) != 4{
 						ctx.JSON(http.StatusOK, swagger.SuccessResp{Result:1, Message:"path corrupt"})
 						return
 					}
+					groupPath := paths[0]
 					ver := paths[1]
 					srv := paths[2]
 					function := paths[3]
 
-					_, err := apigroup.FindApiBySrvFunction(ver, srv, function)
+					apiProxy, err := apigroup.FindApiBySrvFunction(ver, srv, function)
 					if err != nil {
 						ctx.JSON(http.StatusOK, swagger.SuccessResp{Result:1, Message:"not find function"})
 						return
 					}
 
-					// TODO: run test
-					ctx.JSON(http.StatusOK, swagger.SuccessResp{Result:0, Message:"in developing"})
+					ctx.ShouldBindJSON(apiProxy.ApiDocInfo.Input)
+
+					apiErr := gateway.Run("/" + groupPath + apiProxy.ApiDocInfo.Path, apiProxy.ApiDocInfo.Input, apiProxy.ApiDocInfo.Output)
+					if apiErr != nil {
+						fmt.Println("api err: ", apiErr)
+						ctx.JSON(http.StatusOK, swagger.SuccessResp{Result:1, Message:apiErr.ErrMsg})
+						return
+					}
+
+					ctx.JSON(http.StatusOK, apiProxy.ApiDocInfo.Output)
 				})
 
 				swagger.Swagger3(router, apiGroupName, apiProxy.ApiDocInfo.Path,"post", &swagger.StructParam{
